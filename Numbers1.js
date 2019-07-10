88510a79bb0f665608707aae3c294a53edbb3d76
cde123456780 <brysonjackychen@gmail.com>
2019-07-10 13:17:19 +1200

    Create Numbers1.js
    A solution to Numbers - Problem 1 in Javascript

---
+++ Numbers1.js
@@ -0,0 +1,28 @@
+let findDecimalPlaces = function() {
+  //This function asks for the user for the number of decimal places
+  let decimals = "";
+  while (decimals == "") {
+    let userInput = Window.prompt("Enter the number of decimal places: ");
+    if (typeof(userInput) == "number" && userInput > 0) {
+      if (userInput < 49) {
+        decimals = userInput;
+      } else {
+        console.log("Input must be less than 49");
+      }
+
+    } else if (typeof(userInput) != "number") {
+      console.log("Input must be a number");
+    } else {
+      console.log("Input must be greater than 0");
+    }
+  }
+  return decimals;
+}
+
+let printPi = function(decimals) {
+  //This function prints out Pi to the given number of decimal places
+  console.log(Math.PI.toFixed(decimals));
+}
+
+let decimals = findDecimalPlaces()
+printPi(decimals);
